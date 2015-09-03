8a48609d0e070402a048c06f8b94457e572fa83c
Jakob Unterwurzacher <jakobunt@gmail.com>
2015-09-03 19:09:23 +0200

    Add frontend stubs

---
+++ frontend/file.go
@@ -0,0 +1,9 @@
+package frontend
+
+import (
+	"github.com/rfjakob/gocryptfs/cryptfs"
+)
+
+type File struct {
+	cryptfs.File
+}
---
+++ frontend/fs.go
@@ -0,0 +1,9 @@
+package frontend
+
+import (
+	"github.com/rfjakob/gocryptfs/cryptfs"
+)
+
+type FS struct {
+	cryptfs.FS
+}
