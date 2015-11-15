7a2ab0b233fba71fa03c461a94d50c12dcc1d5e3
Jakob Unterwurzacher <jakobunt@gmail.com>
2015-11-15 13:48:30 +0100

    tests: add tests for "-config" option

--- integration_tests/cli_test.go
+++ integration_tests/cli_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/rfjakob/gocryptfs/cryptfs"
 )
 
+// Test -init flag
 func TestInit(t *testing.T) {
 	dir := tmpDir + "TestInit/"
 	err := os.Mkdir(dir, 0777)
@@ -23,23 +24,56 @@ func TestInit(t *testing.T) {
 	}
 	err = cmd.Run()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	_, err = os.Stat(dir + cryptfs.ConfDefaultName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Test -passwd
+	cmd2 := exec.Command(gocryptfsBinary, "-passwd", "-extpass", "echo test", dir)
+	if testing.Verbose() {
+		cmd2.Stdout = os.Stdout
+		cmd2.Stderr = os.Stderr
+	}
+	err = cmd2.Run()
 	if err != nil {
 		t.Error(err)
 	}
 }
 
-// "dir" has been initialized by TestInit
-func TestPasswd(t *testing.T) {
-	dir := tmpDir + "TestInit/"
-	cmd := exec.Command(gocryptfsBinary, "-passwd", "-extpass", "echo test", dir)
+// Test -init & -config flag
+func TestInitConfig(t *testing.T) {
+	dir := tmpDir + "TestInitConfig/"
+	config := tmpDir + "TestInitConfig.conf"
+	err := os.Mkdir(dir, 0777)
+	if err != nil {
+		t.Fatal(err)
+	}
+	cmd := exec.Command(gocryptfsBinary, "-init", "-extpass", "echo test",
+		"-config", config, dir)
 	if testing.Verbose() {
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 	}
-	err := cmd.Run()
+	err = cmd.Run()
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = os.Stat(config)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Test -passwd & -config
+	cmd2 := exec.Command(gocryptfsBinary, "-passwd", "-extpass", "echo test",
+		"-config", config, dir)
+	if testing.Verbose() {
+		cmd2.Stdout = os.Stdout
+		cmd2.Stderr = os.Stderr
+	}
+	err = cmd2.Run()
 	if err != nil {
 		t.Error(err)
 	}
