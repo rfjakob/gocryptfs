8766ab5472bcd3f70cc4caedc83d68cbd8155283
Jakob Unterwurzacher <jakobunt@gmail.com>
2015-11-28 20:21:06 +0100

    tests: verify that symlinks work

--- integration_tests/example_filesystems_test.go
+++ integration_tests/example_filesystems_test.go
@@ -3,6 +3,7 @@ package integration_tests
 // Mount example filesystems and check that the file "status.txt" is there
 
 import (
+	"path/filepath"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -12,14 +13,34 @@ const statusTxtContent = "It works!\n"
 
 // checkStatusTxt - read file "filename" and verify that it contains
 // "It works!\n"
-func checkStatusTxt(t *testing.T, filename string) {
-	contentBytes, err := ioutil.ReadFile(filename)
+func checkExampleContent(t *testing.T, dir string) {
+	// Check regular file
+	statusFile := filepath.Join(dir, "status.txt")
+	contentBytes, err := ioutil.ReadFile(statusFile)
 	if err != nil {
 		t.Fatal(err)
 	}
 	content := string(contentBytes)
 	if content != statusTxtContent {
 		t.Errorf("Unexpected content: %s\n", content)
+	}
+	// Check relative symlink
+	symlink := filepath.Join(dir, "rel")
+	target, err := os.Readlink(symlink)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if target != "status.txt" {
+		t.Errorf("Unexpected link target: %s\n", target)
+	}
+	// Check absolute symlink
+	symlink = filepath.Join(dir, "abs")
+	target, err = os.Readlink(symlink)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if target != "/a/b/c/d" {
+		t.Errorf("Unexpected link target: %s\n", target)
 	}
 }
 
@@ -33,11 +54,11 @@ func TestExampleFsNormal(t *testing.T) {
 		t.Fatal(err)
 	}
 	mount(cDir, pDir, "-extpass", "echo test")
-	checkStatusTxt(t, pDir+"status.txt")
+	checkExampleContent(t, pDir)
 	unmount(pDir)
 	mount(cDir, pDir, "-masterkey", "74676e34-0b47c145-00dac61a-17a92316-"+
 		"bb57044c-e205b71f-65f4fdca-7cabd4b3", "-diriv=false")
-	checkStatusTxt(t, pDir+"status.txt")
+	checkExampleContent(t, pDir)
 	unmount(pDir)
 	err = os.Remove(pDir)
 	if err != nil {
