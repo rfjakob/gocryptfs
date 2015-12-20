38f7dc1a17ee62b2d2bdc407b4deeee8b385dd04
Jakob Unterwurzacher <jakobunt@gmail.com>
2015-12-20 17:30:10 +0100

    Make build.bash version bake-in compatible with Go 1.3 and 1.4

--- build.bash
+++ build.bash
@@ -4,4 +4,15 @@ set -eu
 
 GITVERSION=$(git describe --tags --dirty)
 
-go build -ldflags="-X main.GitVersion=$GITVERSION" && ./gocryptfs -version
+# go version go1.5.1 linux/amd64
+V=$(go version | cut -d" " -f3 | cut -c3-5)
+
+if [ $V == "1.3" -o $V == "1.4" ]
+then
+	go build -ldflags="-X main.GitVersion $GITVERSION"
+else
+	# Go 1.5 wants an "=" here 
+	go build -ldflags="-X main.GitVersion=$GITVERSION"
+fi
+
+./gocryptfs -version
