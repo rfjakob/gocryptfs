cf29ce37627fd6ba4416dd9eb5e56bed44128131
Jakob Unterwurzacher <jakobunt@gmail.com>
2016-05-05 00:02:04 +0200

    stupidgcm: set dummy locking callback.
    In general, OpenSSL is only threadsafe if you provide a locking function
    through CRYPTO_set_locking_callback. However, the GCM operations that
    stupidgcm uses never call that function.
    
    To guard against that ever changing, set a dummy locking callback
    that crashes the app.

---
+++ internal/stupidgcm/locking.go
@@ -0,0 +1,26 @@
+package stupidgcm
+
+// In general, OpenSSL is only threadsafe if you provide a locking function
+// through CRYPTO_set_locking_callback. However, the GCM operations that
+// stupidgcm uses never call that function. Additionally, the manual locking
+// has been removed completely in openssl 1.1.0.
+
+/*
+#include <openssl/evp.h>
+#include <stdio.h>
+
+static void dummy_callback(int mode, int n, const char *file, int line) {
+	printf("stupidgcm: thread locking is not implemented and should not be "
+		"needed. Please upgrade openssl.\n");
+	// panic
+	*((int*)0)=0;
+}
+static void set_dummy_callback() {
+	CRYPTO_set_locking_callback(dummy_callback);
+}
+*/
+import "C"
+
+func init() {
+	C.set_dummy_callback()
+}
