f41d2e067679c4c6a8b0b5dcb52a113918806e48
Jakob Unterwurzacher <jakobunt@gmail.com>
2016-10-24 19:36:44 +0200

    Run cli_args_test.go through "gofmt -s"

--- cli_args_test.go
+++ cli_args_test.go
@@ -14,40 +14,40 @@ type testcase struct {
 
 func TestPrefixOArgs(t *testing.T) {
 	testcases := []testcase{
-		testcase{
+		{
 			i: nil,
 			o: nil,
 		},
-		testcase{
+		{
 			i: []string{"gocryptfs"},
 			o: []string{"gocryptfs"},
 		},
-		testcase{
+		{
 			i: []string{"gocryptfs", "-v"},
 			o: []string{"gocryptfs", "-v"},
 		},
-		testcase{
+		{
 			i: []string{"gocryptfs", "foo", "bar", "-v"},
 			o: []string{"gocryptfs", "foo", "bar", "-v"},
 		},
-		testcase{
+		{
 			i: []string{"gocryptfs", "foo", "bar", "-o", "a"},
 			o: []string{"gocryptfs", "-a", "foo", "bar"},
 		},
-		testcase{
+		{
 			i: []string{"gocryptfs", "foo", "bar", "-o", "a,b,xxxxx"},
 			o: []string{"gocryptfs", "-a", "-b", "-xxxxx", "foo", "bar"},
 		},
-		testcase{
+		{
 			i: []string{"gocryptfs", "foo", "bar", "-d", "-o=a,b,xxxxx"},
 			o: []string{"gocryptfs", "-a", "-b", "-xxxxx", "foo", "bar", "-d"},
 		},
-		testcase{
+		{
 			i: []string{"gocryptfs", "foo", "bar", "-oooo", "a,b,xxxxx"},
 			o: []string{"gocryptfs", "foo", "bar", "-oooo", "a,b,xxxxx"},
 		},
 		// https://github.com/mhogomchungu/sirikali/blob/a36d91d3e39f0c1eb9a79680ed6c28ddb6568fa8/src/siritask.cpp#L192
-		testcase{
+		{
 			i: []string{"gocryptfs", "-o", "rw", "--config", "fff", "ccc", "mmm"},
 			o: []string{"gocryptfs", "-rw", "--config", "fff", "ccc", "mmm"},
 		},
