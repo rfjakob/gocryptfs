477071d673f0d7e91c837459d9098744a5ec7d59
Jakob Unterwurzacher <jakobunt@gmail.com>
2017-02-23 00:04:51 +0100

    speed: fix build for Go 1.4 and lower
    Old Go versions miss cipher.NewGCMWithNonceSize, which causes:
    
      internal/speed/speed.go:95: undefined: cipher.NewGCMWithNonceSize

--- internal/speed/speed.go
+++ internal/speed/speed.go
@@ -1,3 +1,5 @@
+// +build go1.5
+
 // Package speed implements the "-speed" command-line option,
 // similar to "openssl speed".
 // It benchmarks the crypto algorithms and libraries used by
---
+++ internal/speed/speed_go1.4.go
@@ -0,0 +1,11 @@
+// +build !go1.5
+
+package speed
+
+import (
+	"fmt"
+)
+
+func Run() {
+	fmt.Printf("Compile with Go 1.5 or higher to run the benchmarks\n")
+}
