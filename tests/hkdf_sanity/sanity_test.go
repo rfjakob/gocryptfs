f1dbd19fe9f277430f4edc1501f4fb5dd8f745b1
Jakob Unterwurzacher <jakobunt@gmail.com>
2017-03-18 16:48:58 +0100

    tests: add hkdf_sanity tests with broken example filesystem
    These are deliberately corrupt.

---
+++ tests/hkdf_sanity/sanity_test.go
@@ -0,0 +1,34 @@
+// We test two filesystems that have the "HKDF" feature flag in their config file
+// set, but the actual file contents and names are encrypted with HKDF disabled.
+// This test verifies that the "HKDF" feature flag in the config file takes effect.
+package hkdf_sanity
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/rfjakob/gocryptfs/tests/test_helpers"
+)
+
+func TestBrokenContent(t *testing.T) {
+	cDir := "broken_content"
+	pDir := test_helpers.TmpDir + "/" + cDir
+	test_helpers.MountOrFatal(t, cDir, pDir, "-extpass", "echo test", "-wpanic=false")
+	_, err := ioutil.ReadFile(pDir + "/status.txt")
+	if err == nil {
+		t.Error("this should fail")
+	}
+	test_helpers.UnmountPanic(pDir)
+}
+
+func TestBrokenNames(t *testing.T) {
+	cDir := "broken_names"
+	pDir := test_helpers.TmpDir + "/" + cDir
+	test_helpers.MountOrFatal(t, cDir, pDir, "-extpass", "echo test", "-wpanic=false")
+	_, err := os.Stat(pDir + "/status.txt")
+	if err == nil {
+		t.Error("this should fail")
+	}
+	test_helpers.UnmountPanic(pDir)
+}
