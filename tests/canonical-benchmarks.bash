c12a50d4c8c24ff55d682b2fed1923be4f87f182
Jakob Unterwurzacher <jakobunt@gmail.com>
2017-06-27 00:04:58 +0200

    benchmarks: add streaming read benchmark

--- tests/canonical-benchmarks.bash
+++ tests/canonical-benchmarks.bash
@@ -32,16 +32,19 @@ function etime {
 
 echo -n "WRITE: "
 dd if=/dev/zero of=zero bs=131072 count=2000 2>&1 | tail -n 1
+sleep 0.1
+echo -n "READ:  "
+dd if=zero of=/dev/null bs=131072 count=2000 2>&1 | tail -n 1
 rm zero
-sleep 1
+sleep 0.1
 echo -n "UNTAR: "
 etime tar xzf /tmp/linux-3.0.tar.gz
-sleep 1
+sleep 0.1
 echo -n "MD5:   "
 etime md5sum --quiet -c $MD5
-sleep 1
+sleep 0.1
 echo -n "LS:    "
 etime ls -lR linux-3.0
-sleep 1
+sleep 0.1
 echo -n "RM:    "
 etime rm -Rf linux-3.0
