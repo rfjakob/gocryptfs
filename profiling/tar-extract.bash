b256c39271613813b94893e044df43a0730c0c13
Jakob Unterwurzacher <jakobunt@gmail.com>
2017-07-01 11:42:52 +0200

    profiling: add tar-extract helper
    Extracts the linux-3.0.tar.gz tarball while capturing memory
    and cpu profiles.

---
+++ profiling/tar-extract.bash
@@ -0,0 +1,23 @@
+#!/bin/bash -eu
+
+cd "$(dirname "$0")"
+
+# Download /tmp/linux-3.0.tar.gz
+../tests/dl-linux-tarball.bash
+
+T=$(mktemp -d)
+mkdir $T/a $T/b
+
+../gocryptfs -init -quiet -scryptn 10 -extpass "echo test" $T/a
+../gocryptfs -quiet -extpass "echo test" -cpuprofile $T/cprof -memprofile $T/mprof \
+	$T/a $T/b
+
+# Cleanup trap
+trap "cd /; fusermount -u -z $T/b; rm -Rf $T/a" EXIT
+
+echo "Extracting..."
+time tar xzf /tmp/linux-3.0.tar.gz -C $T/b
+
+echo
+echo "Hint: go tool pprof ../gocryptfs $T/cprof"
+echo "      go tool pprof -alloc_space ../gocryptfs $T/mprof"
