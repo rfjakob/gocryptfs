8f92dd15e13552c3968838e9525118fbcef5fb5f
Jakob Unterwurzacher <jakobunt@gmail.com>
2017-09-03 13:50:53 +0200

    profiling: add write-trace.bash
    Save an execution trace of writing 100MB of data
    to a new gocryptfs mount on /tmp

---
+++ profiling/write-trace.bash
@@ -0,0 +1,23 @@
+#!/bin/bash -eu
+#
+# Write an execution trace of writing 100MB of data
+# to a new gocryptfs mount on /tmp
+
+cd "$(dirname "$0")"
+
+T=$(mktemp -d)
+mkdir $T/a $T/b
+
+../gocryptfs -init -quiet -scryptn 10 -extpass "echo test" $T/a
+../gocryptfs -quiet -extpass "echo test" -trace $T/trace \
+	$T/a $T/b
+
+# Cleanup trap
+trap "cd /; fusermount -u -z $T/b; rm -Rf $T/a" EXIT
+
+# Write only 1x100MB, otherwise the trace gets too big.
+dd if=/dev/zero of=$T/b/zero bs=1M count=100
+
+echo
+echo "Hint: go tool trace $T/trace"
+
