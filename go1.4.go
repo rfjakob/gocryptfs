512be8f081d2cc2afc778a2dcf5447647de2bbea
Jakob Unterwurzacher <jakobunt@gmail.com>
2017-09-06 20:52:30 +0200

    Add "dep" files: Gopkg.toml and Gopkg.lock
    Allows users to get a reproduceable build. Still needs to
    be integrated into build.bash.
    
    Suggested at https://github.com/rfjakob/gocryptfs/issues/142

--- go1.4.go
+++ go1.4.go
@@ -4,4 +4,4 @@ package main
 
 // Cause an early compile error on Go 1.4 an lower. We need Go 1.5 for a number
 // of reasons, among them NewGCMWithNonceSize, RawURLEncoding, runtime/trace.
-import "You_need_Go_1.5_or_higher_to_compile_gocryptfs"
+import "You_need_Go_1_5_or_higher_to_compile_gocryptfs"
