f30522a0c14218ad236831d1c229968ea46ec8aa
Sebastian Lackner <sebastian@fds-team.de>
2017-11-30 21:13:46 +0100

    syscallcompat: Fix syscall emulation for absolute paths
    For absolute paths, the file descriptor should be ignored. In such a case
    there is also no need to hold the lock or change the working directory.

--- internal/syscallcompat/emulate.go
+++ internal/syscallcompat/emulate.go
@@ -64,18 +64,20 @@ func emulateRenameat(olddirfd int, oldpath string, newdirfd int, newpath string)
 // emulateUnlinkat emulates the syscall for platforms that don't have it
 // in the kernel (darwin).
 func emulateUnlinkat(dirfd int, path string, flags int) (err error) {
-	chdirMutex.Lock()
-	defer chdirMutex.Unlock()
-	cwd, err := syscall.Open(".", syscall.O_RDONLY, 0)
-	if err != nil {
-		return err
+	if !filepath.IsAbs(path) {
+		chdirMutex.Lock()
+		defer chdirMutex.Unlock()
+		cwd, err := syscall.Open(".", syscall.O_RDONLY, 0)
+		if err != nil {
+			return err
+		}
+		defer syscall.Close(cwd)
+		err = syscall.Fchdir(dirfd)
+		if err != nil {
+			return err
+		}
+		defer syscall.Fchdir(cwd)
 	}
-	defer syscall.Close(cwd)
-	err = syscall.Fchdir(dirfd)
-	if err != nil {
-		return err
-	}
-	defer syscall.Fchdir(cwd)
 	if (flags & unix.AT_REMOVEDIR) != 0 {
 		return syscall.Rmdir(path)
 	} else {
@@ -123,18 +125,20 @@ func dirfdAbs(dirfd int, path string) (string, error) {
 // emulateFchmodat emulates the syscall for platforms that don't have it
 // in the kernel (darwin).
 func emulateFchmodat(dirfd int, path string, mode uint32, flags int) (err error) {
-	chdirMutex.Lock()
-	defer chdirMutex.Unlock()
-	cwd, err := syscall.Open(".", syscall.O_RDONLY, 0)
-	if err != nil {
-		return err
+	if !filepath.IsAbs(path) {
+		chdirMutex.Lock()
+		defer chdirMutex.Unlock()
+		cwd, err := syscall.Open(".", syscall.O_RDONLY, 0)
+		if err != nil {
+			return err
+		}
+		defer syscall.Close(cwd)
+		err = syscall.Fchdir(dirfd)
+		if err != nil {
+			return err
+		}
+		defer syscall.Fchdir(cwd)
 	}
-	defer syscall.Close(cwd)
-	err = syscall.Fchdir(dirfd)
-	if err != nil {
-		return err
-	}
-	defer syscall.Fchdir(cwd)
 	// We also don't have Lchmod, so emulate it (poorly).
 	if flags&unix.AT_SYMLINK_NOFOLLOW != 0 {
 		fi, err := os.Lstat(path)
@@ -151,53 +155,59 @@ func emulateFchmodat(dirfd int, path string, mode uint32, flags int) (err error)
 // emulateFchownat emulates the syscall for platforms that don't have it
 // in the kernel (darwin).
 func emulateFchownat(dirfd int, path string, uid int, gid int, flags int) (err error) {
-	chdirMutex.Lock()
-	defer chdirMutex.Unlock()
-	cwd, err := syscall.Open(".", syscall.O_RDONLY, 0)
-	if err != nil {
-		return err
+	if !filepath.IsAbs(path) {
+		chdirMutex.Lock()
+		defer chdirMutex.Unlock()
+		cwd, err := syscall.Open(".", syscall.O_RDONLY, 0)
+		if err != nil {
+			return err
+		}
+		defer syscall.Close(cwd)
+		err = syscall.Fchdir(dirfd)
+		if err != nil {
+			return err
+		}
+		defer syscall.Fchdir(cwd)
 	}
-	defer syscall.Close(cwd)
-	err = syscall.Fchdir(dirfd)
-	if err != nil {
-		return err
-	}
-	defer syscall.Fchdir(cwd)
 	return syscall.Lchown(path, uid, gid)
 }
 
 // emulateSymlinkat emulates the syscall for platforms that don't have it
 // in the kernel (darwin).
 func emulateSymlinkat(oldpath string, newdirfd int, newpath string) (err error) {
-	chdirMutex.Lock()
-	defer chdirMutex.Unlock()
-	cwd, err := syscall.Open(".", syscall.O_RDONLY, 0)
-	if err != nil {
-		return err
+	if !filepath.IsAbs(newpath) {
+		chdirMutex.Lock()
+		defer chdirMutex.Unlock()
+		cwd, err := syscall.Open(".", syscall.O_RDONLY, 0)
+		if err != nil {
+			return err
+		}
+		defer syscall.Close(cwd)
+		err = syscall.Fchdir(newdirfd)
+		if err != nil {
+			return err
+		}
+		defer syscall.Fchdir(cwd)
 	}
-	defer syscall.Close(cwd)
-	err = syscall.Fchdir(newdirfd)
-	if err != nil {
-		return err
-	}
-	defer syscall.Fchdir(cwd)
 	return syscall.Symlink(oldpath, newpath)
 }
 
 // emulateMkdirat emulates the syscall for platforms that don't have it
 // in the kernel (darwin).
 func emulateMkdirat(dirfd int, path string, mode uint32) (err error) {
-	chdirMutex.Lock()
-	defer chdirMutex.Unlock()
-	cwd, err := syscall.Open(".", syscall.O_RDONLY, 0)
-	if err != nil {
-		return err
+	if !filepath.IsAbs(path) {
+		chdirMutex.Lock()
+		defer chdirMutex.Unlock()
+		cwd, err := syscall.Open(".", syscall.O_RDONLY, 0)
+		if err != nil {
+			return err
+		}
+		defer syscall.Close(cwd)
+		err = syscall.Fchdir(dirfd)
+		if err != nil {
+			return err
+		}
+		defer syscall.Fchdir(cwd)
 	}
-	defer syscall.Close(cwd)
-	err = syscall.Fchdir(dirfd)
-	if err != nil {
-		return err
-	}
-	defer syscall.Fchdir(cwd)
 	return syscall.Mkdir(path, mode)
 }
