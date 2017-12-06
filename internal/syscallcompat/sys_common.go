2ceef01afecafbd4aa80276869993cb53bdadcf4
Jakob Unterwurzacher <jakobunt@gmail.com>
2017-12-07 00:05:28 +0100

    syscallcompat: add Faccessat
    Add faccessat(2) with a hack for symlink, because the
    kernel does not actually looks at the passed flags.
    
    From man 2 faccessat:
    
       C library/kernel differences
           The  raw faccessat() system call takes only the first three argu‐
           ments.  The AT_EACCESS and AT_SYMLINK_NOFOLLOW flags are actually
           implemented  within  the  glibc wrapper function for faccessat().

--- internal/syscallcompat/sys_common.go
+++ internal/syscallcompat/sys_common.go
@@ -1,6 +1,8 @@
 package syscallcompat
 
 import (
+	"syscall"
+
 	"golang.org/x/sys/unix"
 )
 
@@ -20,4 +22,19 @@ func Readlinkat(dirfd int, path string) (string, error) {
 			return string(buf[0:n]), nil
 		}
 	}
+}
+
+// Faccessat exists both in Linux and in MacOS 10.10+, but the Linux version
+// DOES NOT support any flags. Emulate AT_SYMLINK_NOFOLLOW like glibc does.
+func Faccessat(dirfd int, path string, mode uint32) error {
+	var st unix.Stat_t
+	err := Fstatat(dirfd, path, &st, unix.AT_SYMLINK_NOFOLLOW)
+	if err != nil {
+		return err
+	}
+	if st.Mode&syscall.S_IFMT == syscall.S_IFLNK {
+		// Pretend that a symlink is always accessible
+		return nil
+	}
+	return unix.Faccessat(dirfd, path, mode, 0)
 }
