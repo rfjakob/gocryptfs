4b1d080673603e506e3d22a04f7f98f4bc3915e1
Jakob Unterwurzacher <jakobunt@gmail.com>
2019-01-04 19:56:03 +0100

    Update gocryptfs-xray manpage, have MANPAGE-render.bash generate both
    have MANPAGE-render.bash generate both gocryptfs.1 and gocryptfs-xray.1

--- Documentation/MANPAGE-render.bash
+++ Documentation/MANPAGE-render.bash
@@ -3,9 +3,16 @@
 set -eu
 cd $(dirname "$0")
 
-OUT=gocryptfs.1
+# Render Markdown to a proper man(1) manpage
+function render {
+	IN=$1
+	OUT=$2
+	echo "Rendering $IN to $OUT"
+	echo ".\\\" This is a man page. View it using 'man ./$OUT'" > $OUT
+	echo ".\\\"" >> $OUT
+	pandoc MANPAGE.md -s -t man >> $OUT
+}
 
-# Render MANPAGE.md to a proper man(1) manpage
-echo ".\\\" This is a man page. View it using 'man ./$OUT'" > $OUT
-echo ".\\\"" >> $OUT
-pandoc MANPAGE.md -s -t man >> $OUT && man ./gocryptfs.1
+render MANPAGE.md gocryptfs.1
+render MANPAGE-XRAY.md gocryptfs-xray.1
+
