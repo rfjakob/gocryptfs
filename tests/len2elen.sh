fa98664d8a59d17da88eecedfb4aee41656722f5
Jakob Unterwurzacher <jakobunt@gmail.com>
2019-02-17 16:03:43 +0100

    tests: add len2elen.sh
    Check plaintext file name length -> encrypted file name length relation
    
    Example output:
    
    0 22
    2 22
    3 22
    4 22
    5 22
    6 22
    7 22
    8 22
    9 22
    10 22
    11 22
    12 22
    13 22
    14 22
    15 22
    16 43
    17 43
    18 43
    19 43
    20 43
    21 43
    22 43
    23 43
    24 43
    25 43
    26 43
    27 43
    28 43
    29 43
    30 43
    31 43
    32 64
    33 64
    34 64
    35 64
    36 64
    37 64
    38 64
    39 64
    40 64
    41 64
    42 64
    43 64
    44 64
    45 64
    46 64
    47 64
    48 86
    49 86
    50 86
    51 86
    52 86
    53 86
    54 86
    55 86
    56 86
    57 86
    58 86
    59 86
    60 86
    61 86
    62 86
    63 86
    64 107
    65 107
    66 107
    67 107
    68 107
    69 107
    70 107
    71 107
    72 107
    73 107
    74 107
    75 107
    76 107
    77 107
    78 107
    79 107
    80 128
    81 128
    82 128
    83 128
    84 128
    85 128
    86 128
    87 128
    88 128
    89 128
    90 128
    91 128
    92 128
    93 128
    94 128
    95 128
    96 150
    97 150
    98 150
    99 150
    100 150
    101 150
    102 150
    103 150
    104 150
    105 150
    106 150
    107 150
    108 150
    109 150
    110 150
    111 150
    112 171
    113 171
    114 171
    115 171
    116 171
    117 171
    118 171
    119 171
    120 171
    121 171
    122 171
    123 171
    124 171
    125 171
    126 171
    127 171
    128 192
    129 192
    130 192
    131 192
    132 192
    133 192
    134 192
    135 192
    136 192
    137 192
    138 192
    139 192
    140 192
    141 192
    142 192
    143 192
    144 214
    145 214
    146 214
    147 214
    148 214
    149 214
    150 214
    151 214
    152 214
    153 214
    154 214
    155 214
    156 214
    157 214
    158 214
    159 214
    160 235
    161 235
    162 235
    163 235
    164 235
    165 235
    166 235
    167 235
    168 235
    169 235
    170 235
    171 235
    172 235
    173 235
    174 235
    175 235
    176 67
    177 67
    178 67
    179 67
    180 67
    181 67
    182 67
    183 67
    184 67
    185 67
    186 67
    187 67
    188 67
    189 67
    190 67
    191 67
    192 67
    193 67
    194 67
    195 67
    196 67
    197 67
    198 67
    199 67
    200 67
    201 67
    202 67
    203 67
    204 67
    205 67
    206 67
    207 67
    208 67
    209 67
    210 67
    211 67
    212 67
    213 67
    214 67
    215 67
    216 67
    217 67
    218 67
    219 67
    220 67
    221 67
    222 67
    223 67
    224 67
    225 67
    226 67
    227 67
    228 67
    229 67
    230 67
    231 67
    232 67
    233 67
    234 67
    235 67
    236 67
    237 67
    238 67
    239 67
    240 67
    241 67
    242 67
    243 67
    244 67
    245 67
    246 67
    247 67
    248 67
    249 67
    250 67
    251 67
    252 67
    253 67
    254 67
    255 67

---
+++ tests/len2elen.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -eu
+#
+# Check plaintext file name length -> encrypted file name length relation
+#
+# Part of the gocryptfs test suite
+# https://nuetzlich.net/gocryptfs/
+
+NAME="x"
+LEN=0
+
+if [[ ! -f a/gocryptfs.conf ]] ; then
+	echo "fatal: must have gocryptfs dir 'a' mounted at 'b'"
+	exit 1
+fi
+if ! mountpoint b > /dev/null ; then
+	echo "fatal: must have gocryptfs dir 'a' mounted at 'b'"
+	exit 1
+fi
+
+rm -f b/*
+
+while [[ $LEN -le 255 ]]; do
+	touch b/$NAME || break
+	ELEN=$(ls a | wc -L)
+	echo $LEN $ELEN
+	rm b/$NAME
+	NAME="${NAME}x"
+	LEN=${#NAME}
+done
