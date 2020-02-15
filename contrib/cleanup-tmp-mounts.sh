747e99468237caeded8d63741b59393cbbbdfc08
Jakob Unterwurzacher <jakobunt@gmail.com>
2020-02-15 14:38:13 +0100

    Add contrib/cleanup-tmp-mounts.sh
    Useful when you have lots of broken mounts after something in
    the test suite went wrong.

---
+++ contrib/cleanup-tmp-mounts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Umount all FUSE filesystems mounted below /tmp and /var/tmp.
+#
+# Useful when you have lots of broken mounts after something in
+# the test suite went wrong.
+
+set -eu
+
+MOUNTS=$(mount | grep ' type fuse\.' | grep 'on /var/tmp/\|on /tmp/' | cut -d' ' -f 3)
+
+for i in $MOUNTS ; do
+	echo "Unmounting $i"
+	fusermount -u -z "$i"
+done
