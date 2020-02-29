256851899269b779e216d8fd1d0534224d87895e
Jakob Unterwurzacher <jakobunt@gmail.com>
2020-02-29 21:51:34 +0100

    crossbuild.bash: set GOARM=7
    From https://github.com/golang/go/wiki/GoArm :
    
      In cross compilation situations, it is recommended
      that you always set an appropriate GOARM value
      along with GOARCH.
    
    The value seems to default to GOARM=5 if not set
    during cross-compilation.

--- crossbuild.bash
+++ crossbuild.bash
@@ -6,8 +6,11 @@ B="go build -tags without_openssl"
 
 set -x
 
-GOOS=linux  GOARCH=arm   $B
+# See https://github.com/golang/go/wiki/GoArm
+GOOS=linux  GOARCH=arm   GOARM=7 $B
 GOOS=linux  GOARCH=arm64 $B
+
+# MacOS
 GOOS=darwin GOARCH=amd64 $B
 
 # The cross-built binary is not useful on the compile host.
