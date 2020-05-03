3e4545bbac6ed642533ad20723c553df79788eca
Jakob Unterwurzacher <jakobunt@gmail.com>
2020-05-03 20:47:39 +0200

    build-without-openssl.bash: suppress "not a dynamic executable" message
    We redirected the wrong ldd fd to /dev/null. Fix it.

--- build-without-openssl.bash
+++ build-without-openssl.bash
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 
 CGO_ENABLED=0 source ./build.bash -tags without_openssl
 
-if ldd gocryptfs > /dev/null ; then
+if ldd gocryptfs 2> /dev/null ; then
 	echo "build-without-openssl.bash: error: compiled binary is not static"
 	exit 1
 fi
