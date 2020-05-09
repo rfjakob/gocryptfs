f2e8b776f8d2f4cff928e127798fc8c98ca2fbc5
Jakob Unterwurzacher <jakobunt@gmail.com>
2020-05-09 19:18:40 +0200

    main: add "go doc" package comment
    Should show up on https://pkg.go.dev/github.com/rfjakob/gocryptfs?tab=doc
    which currently reads "No documentation available for this package!"

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+// gocryptfs is an encrypted overlay filesystem written in Go.
+// See README.md ( https://github.com/rfjakob/gocryptfs/blob/master/README.md )
+// and the official website ( https://nuetzlich.net/gocryptfs/ ) for details.
 package main
 
 import (
