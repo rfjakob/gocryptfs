74a4accf0cc1fd3265abd8fa53b0721cd72c2158
Jakob Unterwurzacher <jakobunt@gmail.com>
2020-06-21 12:44:32 +0200

    v2api: collect RootNode code in root_node.go

--- internal/fusefrontend/node.go
+++ internal/fusefrontend/node.go
@@ -10,9 +10,6 @@ import (
 	"github.com/hanwen/go-fuse/v2/fs"
 	"github.com/hanwen/go-fuse/v2/fuse"
 
-	"github.com/rfjakob/gocryptfs/internal/contentenc"
-	"github.com/rfjakob/gocryptfs/internal/inomap"
-	"github.com/rfjakob/gocryptfs/internal/nametransform"
 	"github.com/rfjakob/gocryptfs/internal/syscallcompat"
 )
 
@@ -20,45 +17,6 @@ import (
 // in a gocryptfs mount.
 type Node struct {
 	fs.Inode
-}
-
-// RootNode is the root of the filesystem tree of Nodes.
-type RootNode struct {
-	Node
-	// args stores configuration arguments
-	args Args
-	// Filename encryption helper
-	nameTransform nametransform.NameTransformer
-	// Content encryption helper
-	contentEnc *contentenc.ContentEnc
-	// MitigatedCorruptions is used to report data corruption that is internally
-	// mitigated by ignoring the corrupt item. For example, when OpenDir() finds
-	// a corrupt filename, we still return the other valid filenames.
-	// The corruption is logged to syslog to inform the user,	and in addition,
-	// the corrupt filename is logged to this channel via
-	// reportMitigatedCorruption().
-	// "gocryptfs -fsck" reads from the channel to also catch these transparently-
-	// mitigated corruptions.
-	MitigatedCorruptions chan string
-	// IsIdle flag is set to zero each time fs.isFiltered() is called
-	// (uint32 so that it can be reset with CompareAndSwapUint32).
-	// When -idle was used when mounting, idleMonitor() sets it to 1
-	// periodically.
-	IsIdle uint32
-
-	// inoMap translates inode numbers from different devices to unique inode
-	// numbers.
-	inoMap *inomap.InoMap
-}
-
-func NewRootNode(args Args, c *contentenc.ContentEnc, n nametransform.NameTransformer) *RootNode {
-	// TODO
-	return &RootNode{
-		args:          args,
-		nameTransform: n,
-		contentEnc:    c,
-		inoMap:        inomap.New(),
-	}
 }
 
 // path returns the relative plaintext path of this node
--- internal/fusefrontend/root_node.go
+++ internal/fusefrontend/root_node.go
@@ -3,8 +3,49 @@ package fusefrontend
 import (
 	"time"
 
+	"github.com/rfjakob/gocryptfs/internal/contentenc"
+	"github.com/rfjakob/gocryptfs/internal/inomap"
+	"github.com/rfjakob/gocryptfs/internal/nametransform"
 	"github.com/rfjakob/gocryptfs/internal/tlog"
 )
+
+// RootNode is the root of the filesystem tree of Nodes.
+type RootNode struct {
+	Node
+	// args stores configuration arguments
+	args Args
+	// Filename encryption helper
+	nameTransform nametransform.NameTransformer
+	// Content encryption helper
+	contentEnc *contentenc.ContentEnc
+	// MitigatedCorruptions is used to report data corruption that is internally
+	// mitigated by ignoring the corrupt item. For example, when OpenDir() finds
+	// a corrupt filename, we still return the other valid filenames.
+	// The corruption is logged to syslog to inform the user,	and in addition,
+	// the corrupt filename is logged to this channel via
+	// reportMitigatedCorruption().
+	// "gocryptfs -fsck" reads from the channel to also catch these transparently-
+	// mitigated corruptions.
+	MitigatedCorruptions chan string
+	// IsIdle flag is set to zero each time fs.isFiltered() is called
+	// (uint32 so that it can be reset with CompareAndSwapUint32).
+	// When -idle was used when mounting, idleMonitor() sets it to 1
+	// periodically.
+	IsIdle uint32
+	// inoMap translates inode numbers from different devices to unique inode
+	// numbers.
+	inoMap *inomap.InoMap
+}
+
+func NewRootNode(args Args, c *contentenc.ContentEnc, n nametransform.NameTransformer) *RootNode {
+	// TODO
+	return &RootNode{
+		args:          args,
+		nameTransform: n,
+		contentEnc:    c,
+		inoMap:        inomap.New(),
+	}
+}
 
 // reportMitigatedCorruption is used to report a corruption that was transparently
 // mitigated and did not return an error to the user. Pass the name of the corrupt
