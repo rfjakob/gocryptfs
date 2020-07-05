40a31e0c325396d3c515032ad5f87299f297e204
Jakob Unterwurzacher <jakobunt@gmail.com>
2020-07-05 20:22:24 +0200

    v2api: list interfaces to be implemented

--- internal/fusefrontend/node_api_check.go
+++ internal/fusefrontend/node_api_check.go
@@ -4,7 +4,7 @@ import (
 	"github.com/hanwen/go-fuse/v2/fs"
 )
 
-// Check that we have implemented the interfaces
+// Check that we have implemented the fs.Node* interfaces
 var _ = (fs.NodeGetattrer)((*Node)(nil))
 var _ = (fs.NodeLookuper)((*Node)(nil))
 var _ = (fs.NodeReaddirer)((*Node)(nil))
@@ -16,3 +16,16 @@ var _ = (fs.NodeReadlinker)((*Node)(nil))
 var _ = (fs.NodeOpener)((*Node)(nil))
 var _ = (fs.NodeOpendirer)((*Node)(nil))
 var _ = (fs.NodeSetattrer)((*Node)(nil))
+
+/* TODO
+var _ = (fs.NodeStatfser)((*Node)(nil))
+var _ = (fs.NodeGetxattrer)((*Node)(nil))
+var _ = (fs.NodeSetxattrer)((*Node)(nil))
+var _ = (fs.NodeRemovexattrer)((*Node)(nil))
+var _ = (fs.NodeListxattrer)((*Node)(nil))
+var _ = (fs.NodeCopyFileRanger)((*Node)(nil))
+var _ = (fs.NodeMknoder)((*Node)(nil))
+var _ = (fs.NodeLinker)((*Node)(nil))
+var _ = (fs.NodeSymlinker)((*Node)(nil))
+var _ = (fs.NodeRenamer)((*Node)(nil))
+*/
