f54d21c38427e68b598546c1c870a289cb5e99bb
Jakob Unterwurzacher <jakobunt@gmail.com>
2020-08-01 21:13:00 +0200

    v2api/reverse: implement Lookup & Getattr

--- internal/fusefrontend_reverse/node.go
+++ internal/fusefrontend_reverse/node.go
@@ -1,11 +1,77 @@
 package fusefrontend_reverse
 
 import (
+	"context"
+	"syscall"
+
+	"golang.org/x/sys/unix"
+
 	"github.com/hanwen/go-fuse/v2/fs"
+	"github.com/hanwen/go-fuse/v2/fuse"
+
+	"github.com/rfjakob/gocryptfs/internal/syscallcompat"
 )
 
 // Node is a file or directory in the filesystem tree
 // in a `gocryptfs -reverse` mount.
 type Node struct {
 	fs.Inode
+}
+
+// Lookup - FUSE call for discovering a file.
+// TODO handle virtual files
+func (n *Node) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (ch *fs.Inode, errno syscall.Errno) {
+	dirfd, cName, errno := n.prepareAtSyscall(name)
+	if errno != 0 {
+		return
+	}
+	defer syscall.Close(dirfd)
+
+	// Get device number and inode number into `st`
+	st, err := syscallcompat.Fstatat2(dirfd, cName, unix.AT_SYMLINK_NOFOLLOW)
+	if err != nil {
+		return nil, fs.ToErrno(err)
+	}
+
+	// Create new inode and fill `out`
+	ch = n.newChild(ctx, st, out)
+
+	// Translate ciphertext size in `out.Attr.Size` to plaintext size
+	n.translateSize(dirfd, cName, &out.Attr)
+
+	return ch, 0
+}
+
+// GetAttr - FUSE call for stat()ing a file.
+//
+// GetAttr is symlink-safe through use of openBackingDir() and Fstatat().
+func (n *Node) Getattr(ctx context.Context, f fs.FileHandle, out *fuse.AttrOut) (errno syscall.Errno) {
+	// If the kernel gives us a file handle, use it.
+	if f != nil {
+		return f.(fs.FileGetattrer).Getattr(ctx, out)
+	}
+
+	dirfd, cName, errno := n.prepareAtSyscall("")
+	if errno != 0 {
+		return
+	}
+	defer syscall.Close(dirfd)
+
+	st, err := syscallcompat.Fstatat2(dirfd, cName, unix.AT_SYMLINK_NOFOLLOW)
+	if err != nil {
+		return fs.ToErrno(err)
+	}
+
+	// Fix inode number
+	rn := n.rootNode()
+	rn.inoMap.TranslateStat(st)
+	out.Attr.FromStat(st)
+
+	// Translate ciphertext size in `out.Attr.Size` to plaintext size
+	n.translateSize(dirfd, cName, &out.Attr)
+
+	if rn.args.ForceOwner != nil {
+		out.Owner = *rn.args.ForceOwner
+	}
+	return 0
 }
---
+++ internal/fusefrontend_reverse/node_helpers.go
@@ -0,0 +1,67 @@
+package fusefrontend_reverse
+
+import (
+	"context"
+	"path/filepath"
+	"syscall"
+
+	"github.com/hanwen/go-fuse/v2/fs"
+	"github.com/hanwen/go-fuse/v2/fuse"
+)
+
+// translateSize translates the ciphertext size in `out` into plaintext size.
+func (n *Node) translateSize(dirfd int, cName string, out *fuse.Attr) {
+	if out.IsRegular() {
+		rn := n.rootNode()
+		out.Size = rn.contentEnc.PlainSizeToCipherSize(out.Size)
+	} else if out.IsSymlink() {
+		panic("todo: call readlink once it is implemented")
+	}
+}
+
+// Path returns the relative plaintext path of this node
+func (n *Node) Path() string {
+	return n.Inode.Path(n.Root())
+}
+
+// rootNode returns the Root Node of the filesystem.
+func (n *Node) rootNode() *RootNode {
+	return n.Root().Operations().(*RootNode)
+}
+
+// prepareAtSyscall returns a (dirfd, cName) pair that can be used
+// with the "___at" family of system calls (openat, fstatat, unlinkat...) to
+// access the backing encrypted directory.
+//
+// If you pass a `child` file name, the (dirfd, cName) pair will refer to
+// a child of this node.
+// If `child` is empty, the (dirfd, cName) pair refers to this node itself.
+func (n *Node) prepareAtSyscall(child string) (dirfd int, cName string, errno syscall.Errno) {
+	p := n.Path()
+	if child != "" {
+		p = filepath.Join(p, child)
+	}
+	rn := n.rootNode()
+	dirfd, cName, err := rn.openBackingDir(p)
+	if err != nil {
+		errno = fs.ToErrno(err)
+	}
+	return
+}
+
+// newChild attaches a new child inode to n.
+// The passed-in `st` will be modified to get a unique inode number.
+func (n *Node) newChild(ctx context.Context, st *syscall.Stat_t, out *fuse.EntryOut) *fs.Inode {
+	// Get unique inode number
+	rn := n.rootNode()
+	rn.inoMap.TranslateStat(st)
+	out.Attr.FromStat(st)
+	// Create child node
+	id := fs.StableAttr{
+		Mode: uint32(st.Mode),
+		Gen:  1,
+		Ino:  st.Ino,
+	}
+	node := &Node{}
+	return n.NewInode(ctx, node, id)
+}
