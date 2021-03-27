e0981ea59ba9224ddbd96a376fccea758e3e0d7c
Jakob Unterwurzacher <jakobunt@gmail.com>
2021-03-27 15:30:01 +0100

    package-static.bash: add gocryptfs-xray to tarball
    Requested here https://github.com/rfjakob/gocryptfs/issues/558
    and here  https://github.com/rfjakob/gocryptfs/issues/496
    
    Closes https://github.com/rfjakob/gocryptfs/issues/496

--- package-static.bash
+++ package-static.bash
@@ -6,20 +6,24 @@ cd "$(dirname "$0")"
 source build-without-openssl.bash
 
 if ldd gocryptfs > /dev/null ; then
-	echo "error: compiled binary is not static"
+	echo "error: compiled gocryptfs binary is not static"
 	exit 1
 fi
 
-# Build gocryptfs.1 man page
+# Build man pages gocryptfs.1 & gocryptfs-xray.1
 ./Documentation/MANPAGE-render.bash > /dev/null
-cp -a ./Documentation/gocryptfs.1 .
 
 ARCH=$(go env GOARCH)
 OS=$(go env GOOS)
 
-TARGZ=gocryptfs_${GITVERSION}_${OS}-static_${ARCH}.tar.gz
+TARBALL=gocryptfs_${GITVERSION}_${OS}-static_${ARCH}.tar
+TARGZ=$TARBALL.gz
 
-tar --owner=root --group=root -czf "$TARGZ" gocryptfs gocryptfs.1
+tar --owner=root --group=root --create -vf "$TARBALL" gocryptfs 
+tar --owner=root --group=root --append -vf "$TARBALL" -C gocryptfs-xray gocryptfs-xray
+tar --owner=root --group=root --append -vf "$TARBALL" -C Documentation gocryptfs.1 gocryptfs-xray.1
+
+gzip -f "$TARBALL"
 
 echo "Tar created."
 echo "Hint for signing: gpg -u 23A02740 --armor --detach-sig $TARGZ"
