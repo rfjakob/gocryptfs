f92a575f431132c429b9a35edc4ece9f2ff8a177
Jakob Unterwurzacher <jakobunt@gmail.com>
2023-06-05 12:46:22 +0200

    tests/cluster: move PoC tests to poc_test.go

--- tests/cluster/cluster_test.go
+++ tests/cluster/cluster_test.go
@@ -14,8 +14,6 @@ import (
 	"syscall"
 	"testing"
 
-	"golang.org/x/sys/unix"
-
 	"github.com/rfjakob/gocryptfs/v2/internal/contentenc"
 	"github.com/rfjakob/gocryptfs/v2/tests/test_helpers"
 )
@@ -184,38 +182,4 @@ func TestConcurrentCreate(t *testing.T) {
 	go workerThread(mnt1 + "/foo")
 	go workerThread(mnt2 + "/foo")
 	wg.Wait()
-}
-
-// Check that byte-range locks work on an empty file
-func TestFcntlFlock(t *testing.T) {
-	path := test_helpers.TmpDir + "/" + t.Name()
-
-	fd1, err := syscall.Open(path, syscall.O_CREAT|syscall.O_WRONLY|syscall.O_EXCL, 0600)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer syscall.Close(fd1)
-
-	// F_OFD_SETLK locks on the same fd always succeed, so we have to
-	// open a 2nd time.
-	fd2, err := syscall.Open(path, syscall.O_RDWR, 0)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer syscall.Close(fd2)
-
-	lk := unix.Flock_t{
-		Type:   unix.F_WRLCK,
-		Whence: unix.SEEK_SET,
-		Start:  0,
-		Len:    0,
-	}
-	err = unix.FcntlFlock(uintptr(fd1), unix.F_OFD_SETLK, &lk)
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = unix.FcntlFlock(uintptr(fd2), unix.F_OFD_SETLK, &lk)
-	if err == nil {
-		t.Fatal("double-lock succeeded but should have failed")
-	}
 }
