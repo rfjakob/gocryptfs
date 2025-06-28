d60d26f615fd1e7eb53a7701ee4ef68864666ccf
Jakob Unterwurzacher <jakobunt@gmail.com>
2025-06-28 16:21:35 +0200

    tests: reverse: fix NPE in TestXattrList
    Detected by staticcheck

--- tests/reverse/xattr_test.go
+++ tests/reverse/xattr_test.go
@@ -35,7 +35,7 @@ func TestXattrList(t *testing.T) {
 	}
 	val := []byte("xxxxxxxxyyyyyyyyyyyyyyyzzzzzzzzzzzzz")
 	num := 20
-	var namesA map[string]string
+	namesA := make(map[string]string)
 	for i := 1; i <= num; i++ {
 		attr := fmt.Sprintf("user.TestXattrList.%02d", i)
 		err = xattr.LSet(fnA, attr, val)
@@ -49,7 +49,7 @@ func TestXattrList(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	var namesC map[string]string
+	namesC := make(map[string]string)
 	for _, n := range tmp {
 		namesC[n] = string(val)
 	}
