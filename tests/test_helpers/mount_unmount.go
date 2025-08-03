dad4c9fc77f3c5c05396259ef0990c1b338344da
Jakob Unterwurzacher <jakobunt@gmail.com>
2025-08-03 22:49:35 +0200

    test_helpers: send SIGQUIT on mount timeout

--- tests/test_helpers/mount_unmount.go
+++ tests/test_helpers/mount_unmount.go
@@ -95,6 +95,11 @@ func Mount(c string, p string, showOutput bool, extraArgs ...string) error {
 	case <-chanUsr1:
 		// noop
 	case <-time.After(2 * time.Second):
+		fmt.Printf("Timeout, sending SIGQUIT and SIGKILL")
+		cmd.Process.Signal(syscall.SIGQUIT)
+		// SIGQUIT should trigger a backtrace. Give it some time to print it.
+		time.Sleep(time.Second)
+		cmd.Process.Kill()
 		log.Panicf("Timeout waiting for process %d", pid)
 	}
 
