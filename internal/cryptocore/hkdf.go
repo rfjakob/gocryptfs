5633b6ff052a9ce6e367470a42ee82bf10cf2f4e
DMyachin <dmitry.myachin@gmail.com>
2026-01-07 20:47:26 +0300

    Update hkdf.go
    Use hkdf from stable api instead of eXperimental

--- internal/cryptocore/hkdf.go
+++ internal/cryptocore/hkdf.go
@@ -1,10 +1,9 @@
 package cryptocore
 
 import (
+	"crypto/hkdf"
 	"crypto/sha256"
 	"log"
-
-	"golang.org/x/crypto/hkdf"
 )
 
 const (
@@ -19,12 +18,10 @@ const (
 // hkdfDerive derives "outLen" bytes from "masterkey" and "info" using
 // HKDF-SHA256 (RFC 5869).
 // It returns the derived bytes or panics.
-func hkdfDerive(masterkey []byte, info string, outLen int) (out []byte) {
-	h := hkdf.New(sha256.New, masterkey, nil, []byte(info))
-	out = make([]byte, outLen)
-	n, err := h.Read(out)
-	if n != outLen || err != nil {
-		log.Panicf("hkdfDerive: hkdf read failed, got %d bytes, error: %v", n, err)
+func hkdfDerive(masterkey []byte, info string, outLen int) []byte {
+	key, err := hkdf.Key(sha256.New, masterkey, nil, info, outLen)
+	if err != nil {
+		log.Panicf("hkdfDerive: hkdf failed with error: %v", err)
 	}
-	return out
+	return key
 }
