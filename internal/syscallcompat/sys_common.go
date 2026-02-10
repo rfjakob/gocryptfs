rfjakob/gocryptfs

commit ab4c0446d3db0665fc2e9a05c0319541d21811eb

reverse: return ENOTTR when xattr name is not decryptable

internal/syscallcompat/sys_common.go
@@ -2,6 +2,7 @@ package syscallcompat
 
 import (
 	"bytes"
+	"fmt"
 	"syscall"
 
 	"golang.org/x/sys/unix"
@@ -128,6 +129,7 @@ func Fgetxattr(fd int, attr string) (val []byte, err error) {
 // Lgetxattr is a wrapper around unix.Lgetxattr that handles the buffer sizing.
 func Lgetxattr(path string, attr string) (val []byte, err error) {
 	fn := func(buf []byte) (int, error) {
+		fmt.Printf("Lgetxattr(%q, %q, buf %d)\n", path, attr, len(buf))
 		return unix.Lgetxattr(path, attr, buf)
 	}
 	return getxattrSmartBuf(fn)

internal/fusefrontend_reverse/node_xattr.go
@@ -40,7 +40,7 @@ func (n *Node) Getxattr(ctx context.Context, attr string, dest []byte) (uint32, 
 	} else {
 		pAttr, err := rn.decryptXattrName(attr)
 		if err != nil {
-			return 0, syscall.EINVAL
+			return 0, noSuchAttributeError
 		}
 		pData, errno := n.getXAttr(pAttr)
 		if errno != 0 {

commit 61685370b1aaffde25e5427a08509d33d71746fe

syscallcompat: delete accidentially-commited debug output
Fixes: ab4c0446d3db0665fc2e9a05c0319541d21811eb

internal/syscallcompat/sys_common.go
@@ -2,7 +2,6 @@ package syscallcompat
 
 import (
 	"bytes"
-	"fmt"
 	"syscall"
 
 	"golang.org/x/sys/unix"
@@ -129,7 +128,6 @@ func Fgetxattr(fd int, attr string) (val []byte, err error) {
 // Lgetxattr is a wrapper around unix.Lgetxattr that handles the buffer sizing.
 func Lgetxattr(path string, attr string) (val []byte, err error) {
 	fn := func(buf []byte) (int, error) {
-		fmt.Printf("Lgetxattr(%q, %q, buf %d)\n", path, attr, len(buf))
 		return unix.Lgetxattr(path, attr, buf)
 	}
 	return getxattrSmartBuf(fn)